2bd2f9b9c78c9775bf53f7a2d7f8b82ae6580e4b
brandongossen <brandongossen@yahoo.com>
2017-11-03 15:34:12 -0500

    Spring Views Exercise Commit Three (Final)
    -Create a Post class that will post a record from the database
    
    -Give the Post class private properties, a constructor to allow news instance of the class, and getters and setters for a title and body.
    
    -Create views for viewing all the posts and for viewing one individual post.
    
    -Create a subdirectory named posts inside of templates and create two files inside this subdirectory called index.html and show.html.
    
    -Edit the PostsController class to return the views.
    
    -Inside the method that shows an individual post, create a new post object and pass it to the view.
    
    -Inside the method that shows all the posts, create a new array list and add two post objects to it, then pass that list to the view.

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -2,34 +2,47 @@ package com.codeup.blog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 
 public class PostsController {
 
-    @ResponseBody
+
     @GetMapping("/posts")
-    public String indexPage() {
-        return "Index Page";
+    public String indexPage(Model viewModel) {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post postOne = new Post("My Coding Experience", "Learning 8 different languages is tough regardless if they are languages that we speak or languages that we code. Learning code is something beautiful, but it can be frustrating at the same time. Overall there is nothing more gratifying than solving a problem that took hours, days, or even weeks to solve! Once I solve a problem I feel the world is mine!!!");
+
+        Post postTwo = new Post("Kawhi Leonard's Injury", "I miss Kawhi Leonard!!! Please get healthy and come back as soon as possible amigo!!! The city of San Antonio need you!!!");
+
+        posts.add(postOne);
+        posts.add(postTwo);
+
+        viewModel.addAttribute("posts", posts);
+        return "posts/index";
     }
 
-    @ResponseBody
     @GetMapping("/posts/{id}")
-    public String viewIndividualPost(@PathVariable int id) {
-        return "Post Id " + id + ": Post!";
+    public String viewIndividualPost(@PathVariable int id, Model viewModel) {
+        Post post= new Post("My Coding Experience", "Learning 8 different languages is tough regardless if they are languages that we speak or languages that we code. Learning code is something beautiful, but it can be frustrating at the same time. Overall there is nothing more gratifying than solving a problem that took hours, days, or even weeks to solve! Once I solve a problem I feel the world is mine!!!");
+
+        viewModel.addAttribute("post", post);
+        return "posts/show";
     }
 
-    @ResponseBody
     @GetMapping("/posts/create")
         public String viewCreatePostForm() {
             return "Here is the form to create a post!!!";
         }
 
-    @ResponseBody
     @PostMapping("/posts/create")
     public String createAPost() {
     return "Create a new Post!";
