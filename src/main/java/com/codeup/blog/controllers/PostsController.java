06d27de885c4342a1d753cb1a105eba1ead7e826
brandongossen <brandongossen@yahoo.com>
2017-11-05 21:27:51 -0600

    Spring Services Exercise 1st commit
    -Create a new directory called services
    
    -Create a PostSvc class in the services directory and give the class an @Service annotation that tells spring that it is a class that will be managed by spring.
    
    -Have the service keep an array list of posts as a property of the class.
    
    -Create methods in the PostSvc class for finding a post, retrieving all the posts, and creating a post.
    
    -Create a few test (dummy) posts to test the methods.
    
    -Inject the Postsvc it into the posts controller using the @Autowired annotation.
    
    -Use the posts service to pass all the posts and individual posts to the corresponding views.

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -1,6 +1,8 @@
 package com.codeup.blog.controllers;
 
 
+import com.codeup.blog.services.PostSvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,30 +11,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
 public class PostsController {
+    private final PostSvc postSvc;
+
+    @Autowired
+    public PostsController(PostSvc postSvc) {
+        this.postSvc = postSvc;
+    }
 
 
     @GetMapping("/posts")
-    public String indexPage(Model viewModel) {
-        ArrayList<Post> posts = new ArrayList<>();
+    public String viewAllAdds(Model viewModel) {
 
-        Post postOne = new Post("My Coding Experience", "Learning 8 different languages is tough regardless if they are languages that we speak or languages that we code. Learning code is something beautiful, but it can be frustrating at the same time. Overall there is nothing more gratifying than solving a problem that took hours, days, or even weeks to solve! Once I solve a problem I feel the world is mine!!!");
-
-        Post postTwo = new Post("Kawhi Leonard's Injury", "I miss Kawhi Leonard!!! Please get healthy and come back as soon as possible amigo!!! The city of San Antonio need you!!!");
-
-        posts.add(postOne);
-        posts.add(postTwo);
+        List<Post> posts = postSvc.findAllPosts();
 
         viewModel.addAttribute("posts", posts);
+
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String viewIndividualPost(@PathVariable int id, Model viewModel) {
-        Post post= new Post("My Coding Experience", "Learning 8 different languages is tough regardless if they are languages that we speak or languages that we code. Learning code is something beautiful, but it can be frustrating at the same time. Overall there is nothing more gratifying than solving a problem that took hours, days, or even weeks to solve! Once I solve a problem I feel the world is mine!!!");
+
+        Post post = postSvc.findOnePost((long) id);
 
         viewModel.addAttribute("post", post);
         return "posts/show";
---
+++ src/main/java/com/codeup/blog/services/PostSvc.java
@@ -0,0 +1,50 @@
+package com.codeup.blog.services;
+
+import com.codeup.blog.controllers.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("postSvc")
+public class PostSvc {
+    private List<Post> posts = new ArrayList<>();
+
+    public PostSvc() {
+        createDummyPosts();
+    }
+
+    public List<Post> findAllPosts(){
+        return posts;
+    }
+
+    public Post findOnePost(Long id){
+        return posts.get((int) (id - 1));
+    }
+
+    public Post savePost(Post post){
+        post.setId((long) (posts.size() + 1));
+        posts.add(post);
+        return post;
+    }
+
+
+private void createDummyPosts() {
+
+    this.savePost(new Post("My Coding Experience", "Learning 8 different languages is tough regardless if they are languages that we speak or languages that we code. Learning code is something beautiful, but it can be frustrating at the same time. Overall there is nothing more gratifying than solving a problem that took hours, days, or even weeks to solve! Once I solve a problem I feel the world is mine!!!"));
+
+    Post postTwo = new Post("Kawhi Leonard's Injury", "I miss Kawhi Leonard!!! Please get healthy and come back as soon as possible amigo!!! The city of San Antonio need you!!!");
+
+    Post postThree = new Post("Music Is Awesome", "I love all sorts of music, whether it is Rock, Rap, R&B, etc. The way the sounds of the instruments come together to make one beautiful sound amazes me! That's is why I decided to try to play an instrument my self: Drums!!! I'am now the best drummer in the world!");
+
+    Post postFour = new Post("New Dancing Moves", "I was watching an old Michael Jackson music video the other day, and I saw him bust out the moonwalk! All of a sudden I'm in awe wondering how is it possible that this man can walk like he is on the moon without actually being on the moon?!?!?! Is gravity real? Am I just a part of the matrix?!?!?! Sorry got off topic...The point of the matter is you 'Can't Touch This!'");
+
+//    postSvc.savePost(new Post("Music Is Awesome", "I love all sorts of music, whether it is Rock, Rap, R&B, etc. The way the sounds of the instruments come together to make one beautiful sound amazes me! That's is why I decided to try to play an instrument my self: Drums!!! I'am now the best drummer in the world!"));
+
+
+    this.savePost(postTwo);
+    this.savePost(postThree);
+    this.savePost(postFour);
+    }
+
+}
