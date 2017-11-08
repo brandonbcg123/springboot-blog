7d2feb62a61257c6987d118ac92c1678df6ee698
brandongossen <brandongossen@yahoo.com>
2017-11-08 12:41:28 -0600

    Spring Relationships 1st commit
    -Create a User class, with fields for id, firstname, lastname, username, email, and password.
    
    -In the User and Post classes, define the post - user relationship (ManytoOne in the Post model and OnetoMany in the User model).
    
    -Log in to the mysql server and verify the the generated table structure matches.
    
    -Manually insert a user record in the database.
    
    -Create a UsersRepository class as an interface and extend the CrudRepository to inherit the CRUD functionality that the spring framework provides.

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -3,7 +3,7 @@ package com.codeup.blog.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name="posts")
+@Table(name = "posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +14,12 @@ public class Post {
 
     @Column(nullable = false)
     private String body;
+
+
+    @ManyToOne
+//    @JoinColumn(name = "user_id"): The default value will result in this name for the foreign key;
+    //Use the annotation @JoinColumn to specify what the foreign key name you want.
+    private User user;
 
     public Post(){}
 
@@ -26,6 +32,19 @@ public class Post {
         this.id = id;
         this.title = title;
         this.body = body;
+    }
+
+    public Post( String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public Post(Long id, String title, String body, User user) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.user = user;
     }
 
     public String getTitle() {
@@ -50,5 +69,12 @@ public class Post {
 
     public void setId(Long id) {
         this.id = id;
+    }
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
