e5c6f631714a33ca8aee52dc9249d81c87503535
brandongossen <brandongossen@yahoo.com>
2017-11-09 17:00:08 -0600

    Spring Security Second commit
    -Add the spring-boot-starter-security and thymeleaf-extras-springsecurity4 dependencies node into the pom.xml file.
    
    -Create a model called UsersWithRoles that implements the UserDetails and input the information required to authenticate and authorize users.
    
    -In the UsersRepository add a method to find users by their username.
    
    -Create a new service called UserDetailsLoader that loads the authentication and authorization information of users.
    
    Create a configuration class to determine which pages will require authentication, which pages are available to anyone, the path to the login page once created, and the hashing algorithm used to store passwords.

--- src/main/java/com/codeup/blog/repositories/UsersRepository.java
+++ src/main/java/com/codeup/blog/repositories/UsersRepository.java
@@ -4,6 +4,7 @@ import com.codeup.blog.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
+    public User findByUsername(String username);
 }
 
 
