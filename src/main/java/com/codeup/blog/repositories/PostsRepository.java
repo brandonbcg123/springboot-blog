320e1de6e4dc72e93cc1aa742c286ae186f225c0
brandongossen <brandongossen@yahoo.com>
2017-11-15 15:45:06 -0600

    Spring Blog JSON Additions
    -Add a custom Query statement in the PostsRepository to allow a search the users to search for posts based on either the post's title or body.
    
    -Add a method to the PostSvc that calls the search query created in the PostsRepository and returns a list of posts.
    
    -Create a method in the PostsController that calls the searchForPost method from the PostSvc and maps to the View.
    
    -Add the link to create a post in the nav-bar if the user is signed in.
    
    -Move the error message for an invalid login and the message for when a user logs out to the bottom of the page right above the footer.

--- src/main/java/com/codeup/blog/repositories/PostsRepository.java
+++ src/main/java/com/codeup/blog/repositories/PostsRepository.java
@@ -1,7 +1,16 @@
 package com.codeup.blog.repositories;
 
 import com.codeup.blog.models.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostsRepository extends CrudRepository<Post, Long>{
+    @Query(value = "SELECT * FROM posts p WHERE " + "LOWER(p.title) LIKE LOWER(CONCAT('%',:keyword, '%')) OR " + "LOWER(p.body) LIKE LOWER(CONCAT('%',:keyword, '%'))",
+    nativeQuery = true)
+    List<Post> search(@Param("keyword") String keyword);
 }
+
+//    @Query("SELECT p FROM Post p WHERE p.title LIKE ?1 or p.body LIKE ?1")
